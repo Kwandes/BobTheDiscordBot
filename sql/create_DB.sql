2bf6f0603935f1f905376f47a6932a7de57a1165
Jan <jan.bogoryja@gmail.com>
2020-08-02 05:48:15 +0200

    v0.4.3 Add Database scripts and DB logging
    Add sql scripts for setup of the database

---
+++ sql/create_DB.sql
@@ -0,0 +1,39 @@
+#-------------------------------------------
+-- Schema: discord
+-- Project: https://github.com/Kwandes/BobTheDiscordBot
+-- DB version: MySQL 5.7.26
+-- Description: Creation scripts for all the tables
+#-------------------------------------------
+
+DROP SCHEMA IF EXISTS discord;
+CREATE SCHEMA IF NOT EXISTS discord;
+USE discord;
+
+-- Contains per-guild messages called 'tags'
+CREATE TABLE IF NOT EXISTS discord.tag (
+    id          INT AUTO_INCREMENT NOT NULL,
+    guild_id    VARCHAR(64)        NOT NULL,
+    tag_name    VARCHAR(100)       NOT NULL,
+    tag_content VARCHAR(2000)      NOT NULL,
+    CONSTRAINT pk_tag PRIMARY KEY (id)
+);
+
+-- Contains per-guild prefixes used to trigger commands
+CREATE TABLE IF NOT EXISTS discord.prefix (
+    id       INT AUTO_INCREMENT NOT NULL,
+    guild_id VARCHAR(64)        NOT NULL UNIQUE,
+    prefix   VARCHAR(3)         NOT NULL,
+    CONSTRAINT pk_prefix PRIMARY KEY (id),
+    CONSTRAINT uc_guild_id UNIQUE (guild_id)
+);
+
+-- Contains changes done to the tables in this schema. Used by the triggers
+CREATE TABLE IF NOT EXISTS discord.log (
+    id INT AUTO_INCREMENT NOT NULL ,
+    user_id    TEXT,
+    action     VARCHAR(10),
+    table_name VARCHAR(15),
+    log_time   DATETIME(6),
+    data       TEXT,
+    CONSTRAINT ph_log PRIMARY KEY (id)
+)
---
+++ sql/triggers_DB.sql
@@ -0,0 +1,88 @@
+#-------------------------------------------
+-- Schema: discord
+-- Project: https://github.com/Kwandes/BobTheDiscordBot
+-- DB version: MySQL 5.7.26
+-- Description: Creation scripts for all the triggers. The triggers log all of the table changes to discord.log
+#-------------------------------------------
+
+-- -----------------------------------------------------
+-- Table discord.tag
+-- -----------------------------------------------------
+CREATE TRIGGER tr_discord_tag_ins
+    AFTER INSERT
+    ON discord.tag
+    FOR EACH ROW
+    INSERT INTO discord.log(user_id, action, table_name, log_time, data)
+    VALUES (USER(),
+            'insert',
+            'tag',
+            CURRENT_TIME(6),
+            CONCAT(NEW.guild_id, ' | ', NEW.tag_name, ' | ', NEW.tag_content));
+
+CREATE TRIGGER tr_discord_tag_upd
+    BEFORE UPDATE
+    ON discord.tag
+    FOR EACH ROW
+BEGIN
+    INSERT INTO discord.log(user_id, action, table_name, log_time, data)
+    VALUES (USER(),
+            'update',
+            'tag',
+            CURRENT_TIME(6),
+            CONCAT(NEW.guild_id, ' | ', NEW.tag_name, ' | ', NEW.tag_content));
+END;
+
+CREATE TRIGGER tr_discord_tag_del
+    BEFORE DELETE
+    ON discord.tag
+    FOR EACH ROW
+BEGIN
+    INSERT INTO discord.log(user_id, action, table_name, log_time, data)
+    VALUES (USER(),
+            'delete',
+            'tag',
+            CURRENT_TIME(6),
+            CONCAT(OLD.guild_id, ' | ', OLD.tag_name, ' | ', OLD.tag_content));
+END;
+
+-- -----------------------------------------------------
+-- Table discord.prefix
+-- -----------------------------------------------------
+CREATE TRIGGER tr_discord_prefix_ins
+    AFTER INSERT
+    ON discord.prefix
+    FOR EACH ROW
+    INSERT INTO discord.log(user_id, action, table_name, log_time, data)
+    VALUES (USER(),
+            'insert',
+            'prefix',
+            CURRENT_TIME(6),
+            CONCAT(NEW.guild_id, ' | ', NEW.prefix));
+
+CREATE TRIGGER tr_discord_prefix_upd
+    BEFORE UPDATE
+    ON discord.prefix
+    FOR EACH ROW
+BEGIN
+    INSERT INTO discord.log(user_id, action, table_name, log_time, data)
+    VALUES (USER(),
+            'update',
+            'prefix',
+            CURRENT_TIME(6),
+            CONCAT(NEW.guild_id, ' | ', NEW.prefix));
+END;
+
+CREATE TRIGGER tr_discord_prefix_del
+    BEFORE DELETE
+    ON discord.prefix
+    FOR EACH ROW
+BEGIN
+    INSERT INTO discord.log(user_id, action, table_name, log_time, data)
+    VALUES (USER(),
+            'delete',
+            'prefix',
+            CURRENT_TIME(6),
+            CONCAT(OLD.guild_id, ' | ', OLD.prefix));
+END;
+
+
